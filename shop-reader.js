a433d0748988aec96a0ca405b3d16847bc04495d
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2019-07-26 16:54:32 +0200

    Read file in a directory

--- shop-reader.js
+++ shop-reader.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var path = require("path");
 
 fs.readdir('./files', function(err, files) {
 
@@ -6,8 +7,21 @@ fs.readdir('./files', function(err, files) {
 		throw err;
 	}
 
-	console.log(files);
+    console.log("Reading Files...");
+	console.log(files+ "\n");
+    
+    files.forEach(function(fileName) {
+        var file = path.join(__dirname, "files", fileName);
+        var stats = fs.statSync(file);
+        if(stats.isFile() && fileName !== ".DS_Store") {
+
+            fs.readFile(file, "UTF-8", function(err, contents) {
+
+                console.log(contents);
+
+            });
+
+        }
+    });
 
 });
-
-console.log("Reading Files...");
