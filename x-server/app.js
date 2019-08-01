e98f9b22fdd5f3b367aabf5d5be545e380ce5651
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2019-08-01 23:32:19 +0200

    Separate node server and express server, add build express server with a front-end

---
+++ x-server/app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+
+var app = express();
+
+
+app.use(function(req, res, next) {
+    console.log(`${req.method} request for '${req.url}'`);
+    /** A web-site fires GET several times to load web content (you'll see as in console logs).
+     * So without "next()", it won't ask for the next call and response won't be fulfilled;
+     * hence page won't be loaded.
+     */
+	next();
+});
+
+/**
+ * Static files are files that clients download as they are from the server.
+ * In this case, public folder keeps those files. Express, by default does not allow you
+ * to serve static files. You need to enable it using the following built-in middleware.
+ */
+app.use(express.static("./public"));
+
+
+app.listen(3000);
+
+console.log("Express app running on port 3000");
+
+module.exports = app;
